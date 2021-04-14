ed3e4d3393cb0d41416f897f3ee0541a51d2c6d8
Guneet Singh <guneetkhalsa71@gmail.com>
2021-04-14 20:52:52 +0530

    homepage

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+import os,sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+app = Flask(__name__,template_folder='src/templates',static_folder='src/static')
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
